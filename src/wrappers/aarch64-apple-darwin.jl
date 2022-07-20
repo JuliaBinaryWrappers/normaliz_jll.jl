fffb8f3aa0fd85026367103c4899ab437e2589b7
jlbuild <juliabuildbot@gmail.com>
2022-07-20 20:50:01 +0000

    normaliz_jll build 300.900.301+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -5,12 +5,12 @@ using GMP_jll
 using MPFR_jll
 using FLINT_jll
 using nauty_jll
-using CompilerSupportLibraries_jll
+using LLVMOpenMP_jll
 JLLWrappers.@generate_wrapper_header("normaliz")
 JLLWrappers.@declare_library_product(libnormaliz, "@rpath/libnormaliz.3.dylib")
 JLLWrappers.@declare_executable_product(normaliz)
 function __init__()
-    JLLWrappers.@generate_init_header(GMP_jll, MPFR_jll, FLINT_jll, nauty_jll, CompilerSupportLibraries_jll)
+    JLLWrappers.@generate_init_header(GMP_jll, MPFR_jll, FLINT_jll, nauty_jll, LLVMOpenMP_jll)
     JLLWrappers.@init_library_product(
         libnormaliz,
         "lib/libnormaliz.3.dylib",
