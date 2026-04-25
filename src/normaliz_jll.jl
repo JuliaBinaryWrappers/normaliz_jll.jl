6f576e5b2b46f588586b099976ce6751406e4e64
jlbuild <juliabuildbot@gmail.com>
2026-04-25 07:27:44 +0000

    normaliz_jll build 300.1001.504+0

--- src/normaliz_jll.jl
+++ src/normaliz_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("normaliz")
-JLLWrappers.@generate_main_file("normaliz", UUID("6690c6e9-4e12-53b8-b8fd-4bffaef8839f"))
+JLLWrappers.@generate_main_file("normaliz", Base.UUID("6690c6e9-4e12-53b8-b8fd-4bffaef8839f"))
 end  # module normaliz_jll
